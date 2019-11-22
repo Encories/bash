16b78db1e6171fc4d829a56872996aa8fa6cac15
Encories <rubahin@tut.by>
2019-11-22 17:57:04 +0300

    Update ssl.sh

--- ssl.sh
+++ ssl.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 # This is a comment
 
-
-#ur=$(ls /usr/lib/ssl/certs)
 for var in $(ls /usr/lib/ssl/certs)
 do
 ru=$(openssl x509 -noout -issuer -dates -in /usr/lib/ssl/certs/$var)
-#echo $ur 
 echo -e $var
 echo -e $ru
 echo -e "  "
@@ -16,15 +13,11 @@ done
 #!/bin/bash
 # This is a comment
 
-
-#ur=$(ls /usr/lib/ssl/certs)
-#for var in $(ls /usr/lib/ssl/certs)
 root=$(openssl version -d)
 cut="${root:13:12}"
 for var in $(ls $cut/certs)
 do
 ru=$(openssl x509 -noout -issuer -dates -in $cut/certs/$var)
-#echo $ur 
 echo $root "ff"
 echo -e $var
 echo -e $ru
