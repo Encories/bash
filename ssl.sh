661df57ef953968ae46481aa67a46523dde86bcb
Encories <rubahin@tut.by>
2019-11-22 09:51:17 -0500

    Update ssl.sh

--- ssl.sh
+++ ssl.sh
@@ -12,3 +12,21 @@ echo -e $ru
 echo -e "  "
 done
 
+
+#!/bin/bash
+# This is a comment
+
+
+#ur=$(ls /usr/lib/ssl/certs)
+#for var in $(ls /usr/lib/ssl/certs)
+root=$(openssl version -d)
+cut="${root:13:12}"
+for var in $(ls $cut/certs)
+do
+ru=$(openssl x509 -noout -issuer -dates -in $cut/certs/$var)
+#echo $ur 
+echo $root "ff"
+echo -e $var
+echo -e $ru
+echo -e "  "
+done
